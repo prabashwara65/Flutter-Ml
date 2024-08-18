1c7ec7b95c4b67b26e5bc6d3076ae99933350026
prabashwara65 <prabashwara65@gmail.com>
2024-08-18 11:05:54 +0530

    gallerypicker is not working,istead of using that image picker is used

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:gallery_picker/gallery_picker.dart';
-import 'package:gallery_picker/models/media_file.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,49 +19,30 @@ class _HomePageState extends State<HomePage> {
       ),
       body: _buildUI(),
       floatingActionButton: FloatingActionButton(
-        onPressed: _pickMedia,
+        onPressed: _pickImage,
         child: const Icon(Icons.add),
       ),
     );
   }
 
-  Future<void> _pickMedia() async {
-    try {
-      List<MediaFile>? media = await GalleryPicker.pickMedia(context: context, singleMedia: true);
-      
-      if (media != null && media.isNotEmpty) {
-        var mediaFile = media.first;
-        // Verify that mediaFile is indeed a file and get its path
-        var file = await mediaFile.getFile();
-        
-        if (file != null) {
-          print('Selected media path: ${file.path}'); // Debug print
-          if (await file.exists()) {
-            print('File exists and can be accessed');
-            setState(() {
-              selectedMedia = file;
-            });
-          } else {
-            print('File does not exist');
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Selected file does not exist')),
-            );
-          }
-        } else {
-          print('Failed to get the file from media');
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Failed to get the file from media')),
-          );
-        }
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      final file = File(pickedFile.path);
+      if (await file.exists()) {
+        setState(() {
+          selectedMedia = file;
+        });
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('No media selected')),
+          SnackBar(content: Text('Selected file does not exist')),
         );
       }
-    } catch (e) {
-      print('Error picking media: $e');
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to pick image')),
+        SnackBar(content: Text('No image selected')),
       );
     }
   }
