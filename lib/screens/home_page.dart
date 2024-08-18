d7e6b450139bbdf4125d45e48885ccbe88ea71e7
prabashwara65 <prabashwara65@gmail.com>
2024-08-18 10:10:13 +0530

    add permission

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,8 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:gallery_picker/gallery_picker.dart';
 import 'package:gallery_picker/models/media_file.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   File? selectedMedia;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,13 +19,28 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: const Text('Text Recognition'),
       ),
+      body: _buildUI(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           List<MediaFile>? media = await GalleryPicker.pickMedia(context: context, singleMedia: true);
-          // Do something with the selected media if needed
+          
+          if (media != null && media.isNotEmpty) {
+            var data = await media.first.getFile();
+            setState(() {
+              selectedMedia = data;
+            });
+          }
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _buildUI() {
+    return Center(
+      child: selectedMedia == null
+        ? const Text("Pick an image")
+        : Image.file(selectedMedia!, width: 200),
     );
   }
 }
